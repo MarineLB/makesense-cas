49cee3221131a6837aa5ce0445f0d2fd3d8c2cd5
Marine LB <marine.leborgne@live.fr>
2019-11-09 11:57:37 +0100

    MS: add selected styles

--- src/components/tagList.vue
+++ src/components/tagList.vue
@@ -2,11 +2,19 @@
   <div class="tag-list">
     <h3>{{ label }}</h3>
     <p>Selectionne {{ maxSelected }} tags dans la liste</p>
-    <ul class="tag-list__available-tags">
+    <ul class="tag-list__tags tag-list__tags--selected">
+      <li v-for="tag in tags" :key="tag._id" class="tag-list__tag">
+        <button class="tag-list__tag-button tag-list__tag-button--selected">
+          {{tag.name.fr}}
+          <span class="tag-list__tag-icon tag-list__tag-icon--remove"></span>
+        </button>
+      </li>
+    </ul>
+    <ul class="tag-list__tags tag-list__tags--available">
       <li v-for="tag in tags" :key="tag._id" class="tag-list__tag">
         <button class="tag-list__tag-button">
           {{tag.name.fr}}
-          <span class="tag-list__tag-icon"></span>
+          <span class="tag-list__tag-icon tag-list__tag-icon--add"></span>
         </button>
       </li>
     </ul>
@@ -30,14 +38,18 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style  scoped>
-.tag-list__available-tags {
+<style lang="scss" scoped>
+.tag-list__tags {
   list-style-type: none;
-  border: 2px dashed #e8e8e8;
-  padding: 15px;
   display: flex;
   flex-wrap: wrap;
-  border-radius: 5px;
+  padding: 0;
+
+  &--available {
+    border: 2px dashed #e8e8e8;
+    padding: 15px;
+    border-radius: 5px;
+  }
 }
 .tag-list__tag {
   margin: 5px;
@@ -49,20 +61,39 @@ export default {
   color: #979797;
   font-weight: 600;
   font-size: 1rem;
-  padding: 15px 7.5px;
+  padding: 10px 15px;
   border-radius: 50px;
   cursor: pointer;
   display: flex;
   align-items: center;
   font-family: inherit;
+
+  &:hover {
+    background: #979797;
+    color: white;
+  }
+
+  &--selected {
+    background: #fabe3c;
+    border: 2px solid #fabe3c;
+    color: white;
+    font-weight: 800;
+    &:hover {
+      background: white;
+      color: #fabe3c;
+    }
+  }
 }
-.tag-list__tag-button:hover {
-  background: #979797;
-  color: white;
-}
-.tag-list__tag-icon:before {
-  content: "+";
-  margin-left: 5px;
-  font-size: 1rem;
+.tag-list__tag-icon {
+  &:before {
+    margin-left: 5px;
+    font-size: 1rem;
+  }
+  &--add:before {
+    content: "+";
+  }
+  &--remove:before {
+    content: "-";
+  }
 }
 </style>
