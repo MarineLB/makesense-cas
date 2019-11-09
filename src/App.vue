ce5d0982e38c0e4365db7ab4eb5592297a1168f1
Marine LB <marine.leborgne@live.fr>
2019-11-09 11:23:51 +0100

    MS: create tagList component

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    {{tags}}
+    <tagList></tagList>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import tagList from "./components/tagList";
+
+export default {
+  name: "App",
+  components: {
+    tagList
+  },
+  data() {
+    return {
+      tags: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.projects.makesense.org/v1/taxonomies?limit=200&sort=-createdAt&type=legalForm')
+      .then(response => (this.tags = response))
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Raleway", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #0C3944;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/tagList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tag-list">taglist</div>
+</template>
+
+<script>
+export default {
+  name: "TagList",
+  props: {
+    label: String,
+    maxSelected: Number,
+    tags: Array
+  },
+  data() {
+    return {
+      selectedTags: []
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
